6c6d5a72a1de79aa6ddeb15be1cea63d869bfc65
Kushizaki <kushimegu288@gmail.com>
2024-06-25 16:39:35 +0900

    メモ詳細ページの実装

--- memo.rb
+++ memo.rb
@@ -4,13 +4,36 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
+memos = File.open("./sample.json") { |file| JSON.load(file) }
+
 get '/' do
   @title = 'メモアプリ'
-  @memos = File.open("./sample.json") { |file| JSON.load(file) }
+  @memos = memos
   erb :index
 end
 
 get '/new' do
   @title = 'メモアプリ'
   erb :new
+end
+
+memos.each do |key, value|
+  get "/#{key}" do
+    @title = 'メモアプリ'
+    @id = 1
+    @memo = value
+    erb :show
+  end
+
+  get "/#{key}/edit" do
+    @title = 'メモアプリ'
+    @memo = value
+    erb :edit
+  end
+
+end
+
+delete '/:id' do
+  @id = memos.delete(params[:id])
+  redirect to("/")
 end
