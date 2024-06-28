d282b71fbf278da1f21f66deec77fd07c6900f2f
Kushizaki <kushimegu288@gmail.com>
2024-06-28 15:25:10 +0900

    rubocop、erblintに則った構文の修正

--- memo.rb
+++ memo.rb
@@ -5,12 +5,12 @@ require 'sinatra/reloader'
 require 'json'
 
 helpers do
-  def load_memos(file)
-    File.open(file) { |file| JSON.parse(file.read) }
+  def load_memos(source_file)
+    File.open(source_file) { |file| JSON.parse(file.read) }
   end
 
-  def save_memos(file, memos)
-    File.open(file, 'w') do |file|
+  def save_memos(source_file, memos)
+    File.open(source_file, 'w') do |file|
       JSON.dump(memos, file)
     end
   end
@@ -21,7 +21,7 @@ helpers do
 end
 
 get '/memos' do
-  @memos = load_memos("./sample.json")
+  @memos = load_memos('./sample.json')
   erb :index
 end
 
@@ -30,39 +30,39 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  memos = load_memos("./sample.json")
+  memos = load_memos('./sample.json')
   max_memo_id = memos.keys.max.to_i
-  new_memo = { (max_memo_id + 1).to_s => { "title" => escape_html(params[:title]), "content" => escape_html(params[:content]) }}
+  new_memo = { (max_memo_id + 1).to_s => { 'title' => escape_html(params[:title]), 'content' => escape_html(params[:content]) } }
   memos.merge!(new_memo)
-  save_memos("./sample.json", memos)
-  redirect to("/memos")
+  save_memos('./sample.json', memos)
+  redirect to('/memos')
 end
 
-get "/memos/:id" do
+get '/memos/:id' do
   @id = params[:id]
-  @memo = load_memos("./sample.json")[@id.to_s]
+  @memo = load_memos('./sample.json')[@id.to_s]
   erb :show
 end
 
-get "/memos/:id/edit" do
+get '/memos/:id/edit' do
   @id = params[:id]
-  @memo = load_memos("./sample.json")[@id.to_s]
+  @memo = load_memos('./sample.json')[@id.to_s]
   erb :edit
 end
 
-patch "/memos/:id" do
+patch '/memos/:id' do
   @id = params[:id]
-  @memos = load_memos("./sample.json")
-  updated_memo = { @id => { "title" => params[:title], "content" => params[:content] }}
+  @memos = load_memos('./sample.json')
+  updated_memo = { @id => { 'title' => params[:title], 'content' => params[:content] } }
   @memos.update(updated_memo)
-  save_memos("./sample.json", @memos)
-  redirect to("/memos")
+  save_memos('./sample.json', @memos)
+  redirect to('/memos')
 end
 
-delete "/memos/:id" do
+delete '/memos/:id' do
   @id = params[:id]
-  @memos = load_memos("./sample.json")
+  @memos = load_memos('./sample.json')
   @memos.delete(@id.to_s)
-  save_memos("./sample.json", @memos)
-  redirect to("/memos")
+  save_memos('./sample.json', @memos)
+  redirect to('/memos')
 end
